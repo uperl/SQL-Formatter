9455403a5d5f49c28ee98592f6f9681f10a653ec
Graham Ollis <plicease@cpan.org>
2024-10-03 23:18:25 -0600

    Initial structure

---
+++ lib/SQL/Formatter.pm
@@ -0,0 +1,11 @@
+use warnings;
+use 5.020;
+use experimental qw( postderef signatures );
+
+package SQL::Formatter {
+
+  # ABSTRACT: Format SQL using the rust sqlformat library
+}
+
+1;
+
---
+++ t/sql_formatter.t
@@ -0,0 +1,8 @@
+use Test2::V0 -no_srand => 1;
+use SQL::Formatter;
+
+ok 1, 'todo';
+
+done_testing;
+
+
